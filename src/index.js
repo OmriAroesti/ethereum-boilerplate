95a11b48bfcd6f148dd1eb706fcd39911fc0de6a
Roger <aroesti98@gmail.com>
2021-11-29 14:31:58 +0200

    cleanup: Remove some boilerplate

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import "./index.css";
 import QuickStart from "components/QuickStart";
 import { MoralisDappProvider } from "./providers/MoralisDappProvider/MoralisDappProvider";
 
-/** Get your free Moralis Account https://moralis.io/ */
-
 const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
 const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
 
 const Application = () => {
-  const isServerInfo = APP_ID && SERVER_URL ? true : false;
+  const isServerInfo = APP_ID && SERVER_URL;
   if (isServerInfo)
     return (
       <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
